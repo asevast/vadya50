1bac911cc9cca7073df4e9c28c4eff81890fb643
Flesheater <asevast@ya.ru>
2026-03-30 22:37:10 +0300

    fix supabase server import order

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -1,5 +1,5 @@
 import type { Database } from "@/types/supabase";
-import { createClient, type SupabaseClient } from "@supabase/supabase-js";
+import { type SupabaseClient, createClient } from "@supabase/supabase-js";
 
 let кэшАдминКлиента: SupabaseClient<Database, "public"> | null = null;
 
